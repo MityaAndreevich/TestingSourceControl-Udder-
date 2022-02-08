090ce494fb3af53cae2666ace489e089349d9f93
MityaAndreevich <dmitry.logachev@icloud.com>
2022-02-08 16:45:13 +0300

    Initial Commit

---
+++ TestingSourceControl(Udder)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingSourceControl(Udder)
+//
+//  Created by Dmitry Logachev on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
